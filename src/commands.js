17e267bd66722c111d8e57bdc8fa0ad56d4024fd
Florian Goussin <floriang@eyesopen.com>
2019-10-29 08:18:35 -0600

    remove commentted line

--- src/commands.js
+++ src/commands.js
@@ -4,7 +4,6 @@ Cypress.Commands.overwrite('get', (originalFn, selectors) =>
   originalFn(formatSelectors(selectors))
 )
 
-// Cypress.Commands.add('find', { prevSubject: "element" }, (subject, selectors) => {
 Cypress.Commands.overwrite('find', (originalFn, subject, selectors) =>
   originalFn(subject, formatSelectors(selectors))
 )
