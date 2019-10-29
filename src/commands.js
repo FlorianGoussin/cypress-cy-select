27e328dd8de4840f836b1d6c59ba1d8487482f1b
Florian Goussin <floriang@eyesopen.com>
2019-10-29 08:16:56 -0600

    cypress tests pass

--- src/commands.js
+++ src/commands.js
@@ -1,10 +1,11 @@
 import { formatSelectors } from "./lib"
 
-Cypress.Commands.overwrite('get', (originalFn, selectors) => {
-  originalFn(Cypress.$(formatSelectors(selectors)))
-})
+Cypress.Commands.overwrite('get', (originalFn, selectors) =>
+  originalFn(formatSelectors(selectors))
+)
 
-Cypress.Commands.overwrite('find', { prevSubject: "element" }, (originalFn, subject, selectors) => {
-  cy.wrap(subject.find(formatSelectors(selectors)))
-})
+// Cypress.Commands.add('find', { prevSubject: "element" }, (subject, selectors) => {
+Cypress.Commands.overwrite('find', (originalFn, subject, selectors) =>
+  originalFn(subject, formatSelectors(selectors))
+)
 
--- test/cypress/integration/test.js
+++ test/cypress/integration/test.js
@@ -3,14 +3,21 @@ describe("Test cypress overwritten commands", () => {
     cy.visit("./index.html")
   })
 
-  it.only("should get the element", () => {
+  it("should get the element", () => {
     cy.get("body > .modal")
-    cy.get("body > .modal -cy:my-custom-selector > .some-class").should("exist")
+    cy.get("body > .modal -cy:my-custom-selector")
+    cy.get("body > .modal -cy:my-custom-selector > .some-class")
     cy.get("body > .modal -cy:my-custom-selector > .some-class -cy:my-custom-selector-child -cy:my-custom-selector-grand-child").should("exist")
   })
 
   it("should get and find element", () => {
-    cy.get("body > .modal").find("-cy:my-custom-selector > .some-class").find("-cy:my-custom-selector-child -cy:my-custom-selector-grand-child")
-    cy.get("body > .modal").find("-cy:my-custom-selector > .some-class").find("-cy:my-custom-selector-child").find("-cy:my-custom-selector-grand-child")
+    cy.get("body > .modal")
+      .find("-cy:my-custom-selector > .some-class")
+      .find("-cy:my-custom-selector-child -cy:my-custom-selector-grand-child")
+    cy.get("body > .modal")
+      .find("-cy:my-custom-selector > .some-class")
+      .find("-cy:my-custom-selector-child")
+      .find("-cy:my-custom-selector-grand-child")
+      .should("exist")
   })
 })
