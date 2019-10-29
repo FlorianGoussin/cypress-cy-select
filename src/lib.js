9598489115720721ea64155d5a11822c0de9a26a
Florian Goussin <floriang@eyesopen.com>
2019-10-28 20:10:47 -0600

    refactor - attrSelector is more meaningful than match

--- src/lib.js
+++ src/lib.js
@@ -9,12 +9,12 @@ export const formatSelectors = (selectors, regExp = /(^-.+\:)/) => {
   selectors
   .split(" ")
   .forEach((selector) => {
-    let match = selector.match(regExp)
+    const match = selector.match(regExp)
     if (match) {
-      match = match.pop()
-      const attr = "data" + match.slice(0, -1)
-      const value = selector.replace(match, "")
-      selectors = selectors.replace(`${match}${value}`, `[${attr}=\"${value}\"]`)
+      const attrSelector = match.pop()
+      const attr = "data" + attrSelector.slice(0, -1)
+      const value = selector.replace(attrSelector, "")
+      selectors = selectors.replace(`${attrSelector}${value}`, `[${attr}=\"${value}\"]`)
     }
   })
   return selectors
